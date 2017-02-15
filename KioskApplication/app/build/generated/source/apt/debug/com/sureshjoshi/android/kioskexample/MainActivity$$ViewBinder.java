0bae5b5131da184917dc46b86b1523642308614e
Thomas Pomme <Thomas.benhamou@hotmail.fr>
2017-02-16 00:32:42 +0100

    improve main layout

--- KioskApplication/app/build/generated/source/apt/debug/com/sureshjoshi/android/kioskexample/MainActivity$$ViewBinder.java
+++ KioskApplication/app/build/generated/source/apt/debug/com/sureshjoshi/android/kioskexample/MainActivity$$ViewBinder.java
@@ -8,8 +8,8 @@ import butterknife.ButterKnife.ViewBinder;
 public class MainActivity$$ViewBinder<T extends com.sureshjoshi.android.kioskexample.MainActivity> implements ViewBinder<T> {
   @Override public void bind(final Finder finder, final T target, Object source) {
     View view;
-    view = finder.findRequiredView(source, 2131230721, "field 'mWebView'");
-    target.mWebView = finder.castView(view, 2131230721, "field 'mWebView'");
+    view = finder.findRequiredView(source, 2131296257, "field 'mWebView'");
+    target.mWebView = finder.castView(view, 2131296257, "field 'mWebView'");
   }
 
   @Override public void unbind(T target) {
